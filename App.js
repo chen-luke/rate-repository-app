16a0266d60af0118f53fe5a4f1749e64954bf324
Luke Chen <72319988+chen-luke@users.noreply.github.com>
2026-01-31 21:38:02 -0500

    added routing navigation (#6)

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeRouter } from 'react-router-native';
+
 import Main from './src/components/Main';
 
-export default function App() {
-  return <Main />;
-}
+const App = () => {
+  return (
+    <>
+      <NativeRouter>
+        <Main />
+      </NativeRouter>
+      <StatusBar style='auto' />
+    </>
+  );
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
+export default App;
