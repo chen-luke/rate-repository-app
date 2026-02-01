cf4bc6c1ccbf6f99fc58e4a23a2657dd52dcccaa
Luke Chen <72319988+chen-luke@users.noreply.github.com>
2026-01-31 22:43:20 -0500

    created signIn component (#8)
    * created signIn component
    
    * added red warning color to theme
    
    implemented formik to handle form submissions
    
    added yup validation to formik

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,101 @@
+import { Pressable, TextInput, View, StyleSheet } from 'react-native';
 import Text from './Text';
+import theme from '../theme';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    gap: 10,
+    backgroundColor: 'white',
+    padding: 20,
+  },
+  textInput: {
+    height: 50,
+    borderWidth: 1,
+    borderColor: theme.colors.textSecondary,
+    borderRadius: 5,
+    padding: 10,
+  },
+  submitButton: {
+    height: 50,
+    backgroundColor: theme.colors.primary,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
+
+const initialValues = {
+  username: '',
+  password: '',
+};
+
+const validationSchema = yup.object().shape({
+  username: yup
+    .string()
+    .min(1, 'Username must be greater or equal to 1 character')
+    .required('Username is required'),
+  password: yup
+    .string()
+    .min(4, 'Password length must be greater or equal than 4 characters')
+    .required('Password is required'),
+});
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const handleLogin = (values) => {
+    console.log('Logged in with:', values.username, values.password);
+    // This is where you'd call your API
+  };
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: handleLogin,
+  });
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        placeholder='Username'
+        style={styles.textInput}
+        value={formik.values.username}
+        onChangeText={formik.handleChange('username')}
+      />
+      {formik.touched.username && formik.errors.username && (
+        <Text style={{ color: theme.colors.warn }}>
+          {formik.errors.username}
+        </Text>
+      )}
+      <TextInput
+        placeholder='Password'
+        secureTextEntry
+        style={styles.textInput}
+        value={formik.values.password}
+        onChangeText={formik.handleChange('password')}
+      />
+      {formik.touched.password && formik.errors.password && (
+        <Text style={{ color: theme.colors.warn }}>
+          {formik.errors.password}
+        </Text>
+      )}
+
+      {/* Custom Button using Pressable */}
+      <Pressable
+        style={({ pressed }) => [
+          styles.submitButton,
+          { opacity: pressed ? 0.7 : 1 }, // Visual feedback when tapped
+        ]}
+        onPress={formik.handleSubmit}
+      >
+        <Text style={styles.buttonText}>Sign in</Text>
+      </Pressable>
+    </View>
+  );
 };
 
 export default SignIn;
