684ff668a478c5a39f2f8e7597738ad524084011
chen-luke <72319988+chen-luke@users.noreply.github.com>
2026-01-31 21:37:44 -0500

    added routing navigation

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,7 @@
-import Constants from 'expo-constants';
+// import Constants from 'expo-constants';
 import { StyleSheet, View } from 'react-native';
-import Text from './Text';
 import RepositoryList from './RepositoryList';
+import { Route, Routes, Navigate } from 'react-router-native';
 import AppBar from './AppBar';
 import theme from '../theme';
 
@@ -18,7 +18,10 @@ const Main = () => {
   return (
     <View style={styles.container}>
       <AppBar />
-      <RepositoryList />
+      <Routes>
+        <Route path='/' element={<RepositoryList />} />
+        <Route path='*' element={<Navigate to='/' replace />} />
+      </Routes>
     </View>
   );
 };
