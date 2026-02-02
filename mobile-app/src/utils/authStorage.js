82037da24d08da28f50a5d2bcab7ee2f73ab0fcc
chen-luke <72319988+chen-luke@users.noreply.github.com>
2026-02-02 11:00:55 -0500

    implemtned authStorage

---
+++ mobile-app/src/utils/authStorage.js
@@ -0,0 +1,43 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+class AuthStorage {
+  constructor(namespace = 'auth') {
+    this.namespace = namespace;
+  }
+
+  async getAccessToken() {
+    // Get the access token for the storage
+    try {
+      const token = await AsyncStorage.getItem(`${this.namespace}:token`);
+      return token;
+    } catch (error) {
+      console.log(error);
+      console.log('Failed to set get token');
+    }
+  }
+
+  async setAccessToken(accessToken) {
+    // Add the access token to the storage
+    try {
+      await AsyncStorage.setItem(
+        `${this.namespace}:token`,
+        JSON.stringify(accessToken),
+      );
+    } catch (error) {
+      console.log(error);
+      console.log('Failed to set access token');
+    }
+  }
+
+  async removeAccessToken() {
+    // Remove the access token from the storage
+    try {
+      await AsyncStorage.removeItem(`${this.namespace}:token`);
+    } catch (error) {
+      console.log(error);
+      console.log('Failed to remove access token');
+    }
+  }
+}
+
+export default AuthStorage;
