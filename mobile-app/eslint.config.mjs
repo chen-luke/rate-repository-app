20f730b1d89a24f146575f77a8ff70bdb24442ac
Luke Chen <72319988+chen-luke@users.noreply.github.com>
2026-02-03 16:56:48 -0500

    Revert "Test mobile lecture 1 (#13)"
    This reverts commit be540ec40666440b579e03c384dbf0a147914017.

--- mobile-app/eslint.config.mjs
+++ mobile-app/eslint.config.mjs
@@ -1,52 +1,44 @@
-import { defineConfig } from 'eslint/config';
-import react from 'eslint-plugin-react';
-import reactNative from 'eslint-plugin-react-native';
-import babelParser from '@babel/eslint-parser';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
-import globals from 'globals'; // 1. Import globals
+import { defineConfig } from "eslint/config";
+import react from "eslint-plugin-react";
+import reactNative from "eslint-plugin-react-native";
+import babelParser from "@babel/eslint-parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
 });
 
-export default defineConfig([
-  ...compat.extends(
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:jest/recommended',
-  ), // 2. Add Jest recommended here
+export default defineConfig([{
+    extends: compat.extends("eslint:recommended", "plugin:react/recommended"),
 
-  {
     plugins: {
-      react,
-      'react-native': reactNative,
+        react,
+        "react-native": reactNative,
     },
 
     languageOptions: {
-      globals: {
-        ...reactNative.environments['react-native'].globals,
-        ...globals.jest, // 3. Add Jest globals here
-      },
+        globals: {
+            ...reactNative.environments["react-native"]["react-native"],
+        },
 
-      parser: babelParser,
+        parser: babelParser,
     },
 
     settings: {
-      react: {
-        version: 'detect',
-      },
+        react: {
+            version: "detect",
+        },
     },
 
     rules: {
-      'react/prop-types': 'off',
-      'react/react-in-jsx-scope': 'off',
+        "react/prop-types": "off",
+        "react/react-in-jsx-scope": "off",
     },
-  },
-]);
+}]);
